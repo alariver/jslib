c6892e93b1ff493aae4ac34f68d7d5f276b9cd9c
He Changzheng <sams.march@gmail.com>
2017-10-27 16:39:33 +0800

    modify use cdn.rawgit.com

--- images/templates/bookmarklet_launcher.js
+++ images/templates/bookmarklet_launcher.js
@@ -3,6 +3,6 @@
 		myBookmarklet();
 	}   
 	else {
-		document.body.appendChild(document.createElement('script')).src='http://localhost:8000/static/js/bookmarklet.js?r='+Math.floor(Math.random()*99999999999999999999)
+		document.body.appendChild(document.createElement('script')).src='https://cdn.rawgit.com/alariver/jslib/master/images/static/js/bookmarklet.js?r='+Math.floor(Math.random()*99999999999999999999)
 	}
 })();
